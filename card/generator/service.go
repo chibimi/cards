0a9626364780423319febb0380a2b38ac1262a82
Romain Baugue <romain.baugue@elwinar.com>
2020-02-02 14:12:58 +0100

    Cleanup and preparation of the generation endpoint

--- card/generator/service.go
+++ card/generator/service.go
@@ -1,11 +1,15 @@
 package generator
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/chibimi/cards/card"
-	"github.com/google/uuid"
-	"github.com/jung-kurt/gofpdf"
+	"github.com/julienschmidt/httprouter"
 )
 
 type Service struct {
@@ -20,24 +24,57 @@ func NewService(cards *card.SService, assets string) *Service {
 	}
 }
 
-func (s *Service) GeneratePDF(references []int, lang string) (string, error) {
-	id := uuid.New()
-
-	// generator.new
-	g := NewGenerator(s.src, references, lang, s.assets)
-	g.pdf = gofpdf.New("L", "mm", "letter", "")
-	g.unicode = g.pdf.UnicodeTranslatorFromDescriptor("")
-	// generator.translate
-	err := g.GeneratePDF()
-	if err != nil {
-		return "", err
+func (s *Service) GenerateEndpoint(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var refs []int
+	for _, v := range strings.Split(r.FormValue("cards"), ",") {
+		ref, err := strconv.Atoi(v)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		refs = append(refs, ref)
 	}
 
-	res := fmt.Sprintf("%s/%s.pdf", s.assets, id)
-	err = g.WritePDF(res)
+	res, err := s.Generate(refs, r.FormValue("lang"))
 	if err != nil {
-		return "", err
+		log15.Error("generating cards", "err", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	return res, nil
+	w.Header().Set("Content-type", "application/pdf")
+	_, err := io.Copy(w, res)
+	if err != nil {
+		log15.Error("sending cards", "err", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Service) Generate(refs []int, lang string) (io.Reader, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *Service) DisplayEndpoint(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var refs []int
+	for _, v := range strings.Split(r.FormValue("cards"), ",") {
+		ref, err := strconv.Atoi(v)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		refs = append(refs, ref)
+	}
+
+	var lang = r.FormValue("lang")
+
+	for _, ref := range refs {
+		c, err := s.Build(ref)
+		if err != nil {
+			return nil, fmt.Errorf("building ref %d: %w", ref, err)
+		}
+		cards = append(cards, c...)
+	}
+
+	return nil, nil
 }
