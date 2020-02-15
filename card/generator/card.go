eeb5fdc52c59e4e2804e6d5aca1e10a41a309f00
Romain Baugue <romain.baugue@elwinar.com>
2020-02-15 18:19:04 +0100

    Refactor card building and start handling the profile card

---
+++ card/generator/card.go
@@ -0,0 +1,286 @@
+package generator
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/chibimi/cards/card/ability"
+	multierror "github.com/hashicorp/go-multierror"
+	"gitlab.com/golang-commonmark/markdown"
+)
+
+func (s *Service) Build(r Reference) (cards []Card, err error) {
+	var errs *multierror.Error
+
+	// Build the profile card.
+	profile := ProfileCard{
+		Faction:  Faction(r.Ref.FactionID),
+		Title:    r.Ref.Title,
+		Tagline:  r.Ref.Properties,
+		Portrait: strconv.Itoa(r.Ref.ID),
+	}
+
+	for _, m := range r.Models {
+		profile.Profiles = append(profile.Profiles, Profile{
+			Name: m.Title,
+			Stats: map[string]string{
+				"SPD":      m.SPD,
+				"STR":      m.STR,
+				"MAT":      m.MAT,
+				"RAT":      m.RAT,
+				"DEF":      m.DEF,
+				"ARM":      m.ARM,
+				"CMD":      m.CMD,
+				"Resource": m.Resource,
+				"Base":     m.BaseSize,
+			},
+			Advantages: makeAdvantages(m.Advantages, r.Lang),
+		})
+	}
+
+	cards = append(cards, profile)
+
+	// Build the rules card.
+	// This start with the abilities that refer to the reference itself,
+	// which don't have a source displayed on the card. Example: partisan,
+	// etc. Then, for each model of the reference, the model abilities then
+	// the model weapons' abilities, each set being a different set.
+	rules := RulesCard{
+		Faction: Faction(r.Ref.FactionID),
+		Title:   r.Ref.Title,
+	}
+
+	addAbilities := func(source string, abilities []ability.Ability) {
+		if len(abilities) == 0 {
+			return
+		}
+
+		var err error
+		list := Abilities{
+			Source: source,
+		}
+		for _, a := range abilities {
+			ability := Ability{
+				Name: a.Name,
+				Type: AbilityType(a.Type),
+			}
+			ability.Description, err = s.Compile(fmt.Sprintf(`**%s –** %s`, a.Title, a.Description), r.Lang, a.Title)
+			errs = multierror.Append(errs, err)
+
+			list.Abilities = append(list.Abilities, ability)
+		}
+		rules.Abilities = append(rules.Abilities, list)
+	}
+
+	addAbilities("", r.RefAbilities)
+	for _, m := range r.Models {
+		addAbilities(m.Title, r.ModelsAbilities[m.ID])
+		for _, w := range r.ModelsWeapons[m.ID] {
+			addAbilities(w.Name, r.WeaponsAbilities[w.ID])
+		}
+	}
+
+	cards = append(cards, rules)
+
+	// Build the spells card. It is only a separate card for warboss-type
+	// references.
+	if r.HasSpells() {
+		spells := SpellsCard{
+			Faction: Faction(r.Ref.FactionID),
+			Title:   r.Ref.Title,
+		}
+
+		for _, sp := range r.Spells {
+			spell := Spell{
+				Name: sp.Name,
+				Stats: map[string]string{
+					"COST": sp.Cost,
+					"RNG":  sp.RNG,
+					"AOE":  sp.AOE,
+					"POW":  sp.POW,
+					"DUR":  sp.DUR,
+					"OFF":  sp.OFF,
+				},
+			}
+			spell.Description, err = s.Compile(sp.Description, r.Lang, sp.Name)
+			errs = multierror.Append(errs, err)
+
+			spells.Spells = append(spells.Spells, spell)
+		}
+
+		cards = append(cards, spells)
+	}
+
+	// Build the feat card, if the reference is a warboss-type reference.
+	if r.HasFeat() {
+		feat := FeatCard{
+			Faction: Faction(r.Ref.FactionID),
+			Title:   r.Ref.Title,
+			Name:    r.Feat.Name,
+			Fluff:   r.Feat.Fluff,
+		}
+		feat.Description, err = s.Compile(r.Feat.Description, r.Lang, r.Feat.Name)
+		errs = multierror.Append(errs, err)
+
+		cards = append(cards, feat)
+	}
+
+	return cards, errs.ErrorOrNil()
+}
+
+// reAbility is the regex for abilities tags in texts.
+var reAbility = regexp.MustCompile(`#[0-9]+:[^#]+#`)
+
+// reAdvantage is the regex for advantages tags in texts.
+var reAdvantage = regexp.MustCompile(`:[^: ]+:`)
+
+// Compile takes a strings and returns a HTML version of it
+// with abilities and advantages tags replaced by their textual
+// versions.
+func (s *Service) Compile(src, lang, this string) (string, error) {
+	var abilities []string
+	var errs *multierror.Error
+
+	src = strings.Replace(src, `#this#`, this, -1)
+
+	var buf strings.Builder
+	buf.WriteString(reAbility.ReplaceAllStringFunc(src, func(tag string) string {
+		id, err := strconv.Atoi(tag[1:strings.IndexRune(tag, ':')])
+		if err != nil {
+			errs = multierror.Append(errs, err)
+			return tag
+		}
+
+		ability, err := s.src.Ability.Get(id, lang)
+		if err != nil {
+			errs = multierror.Append(errs, err)
+			return tag
+		}
+
+		abilities = append(abilities, fmt.Sprintf("%s: %s", ability.Name, ability.Description))
+		return ability.Name
+	}))
+	for _, ability := range abilities {
+		buf.WriteString(fmt.Sprintf(" _(%s)_", ability))
+	}
+
+	res := reAdvantage.ReplaceAllStringFunc(buf.String(), func(tag string) string {
+		advantage, err := s.src.Advantage.Get(tag[1 : len(tag)-1])
+		if err != nil {
+			errs = multierror.Append(errs, err)
+			return tag
+		}
+
+		return fmt.Sprintf(`%s ![%s](%s/icons/%s.png)`, advantage.Name, advantage.ID, s.assets, advantage.ID)
+	})
+
+	return markdown.New().RenderToString([]byte(res)), errs.ErrorOrNil()
+}
+
+type Card interface {
+	Type() string
+}
+
+type ProfileCard struct {
+	Faction  Faction
+	Title    string
+	Tagline  string
+	Portrait string
+	Profiles []Profile
+}
+
+func (ProfileCard) Type() string {
+	return "profile"
+}
+
+type Profile struct {
+	Name       string
+	Stats      map[string]string
+	Advantages []Advantage
+	Weapons    []Weapon
+}
+
+type Weapon struct {
+	Name       string
+	Type       WeaponType
+	Number     int
+	System     rune
+	Stats      map[string]string
+	Advantages []Advantage
+}
+
+type WeaponType string
+
+const (
+	WeaponTypeMelee  WeaponType = "melee"
+	WeaponTypeRanged            = "ranged"
+	WeaponTypeMount             = "mount"
+)
+
+type Advantage struct {
+	ID   string
+	Name string
+}
+
+func makeAdvantages(advantages []string, lang string) []Advantage {
+	var res []Advantage
+	for _, id := range advantages {
+		res = append(res, Advantage{
+			ID: id,
+		})
+	}
+	return res
+}
+
+type RulesCard struct {
+	Faction   Faction
+	Title     string
+	Abilities []Abilities
+}
+
+func (RulesCard) Type() string {
+	return "rules"
+}
+
+type Abilities struct {
+	Source    string
+	Abilities []Ability
+}
+
+type Ability struct {
+	Name        string
+	Type        AbilityType
+	Description string
+}
+
+type AbilityType int
+
+type SpellsCard struct {
+	Faction Faction
+	Title   string
+	Spells  []Spell
+}
+
+func (SpellsCard) Type() string {
+	return "spells"
+}
+
+type Spell struct {
+	Name        string
+	Stats       map[string]string
+	Description string
+}
+
+type FeatCard struct {
+	Faction     Faction
+	Title       string
+	Name        string
+	Fluff       string
+	Description string
+}
+
+func (FeatCard) Type() string {
+	return "feat"
+}
--- card/generator/service.go
+++ card/generator/service.go
@@ -7,29 +7,14 @@ import (
 	"io"
 	"net/http"
 	"path"
-	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/chibimi/cards/card"
-	"github.com/chibimi/cards/card/ability"
-	"github.com/chibimi/cards/card/feat"
-	"github.com/chibimi/cards/card/model"
-	"github.com/chibimi/cards/card/reference"
-	"github.com/chibimi/cards/card/spell"
-	"github.com/chibimi/cards/card/weapon"
-	multierror "github.com/hashicorp/go-multierror"
 	"github.com/julienschmidt/httprouter"
 	"github.com/prometheus/common/log"
-	"gitlab.com/golang-commonmark/markdown"
 	log15 "gopkg.in/inconshreveable/log15.v2"
 )
-
-// reAbility is the regex for abilities tags in texts.
-var reAbility = regexp.MustCompile(`#[0-9]+:[^#]+#`)
-
-// reAdvantage is the regex for advantages tags in texts.
-var reAdvantage = regexp.MustCompile(`:[^: ]+:`)
 
 type Service struct {
 	src    *card.SService
@@ -93,11 +78,18 @@ func (s *Service) DisplayEndpoint(w http.ResponseWriter, r *http.Request, p http
 	// simpler to handle.
 	var cards = make([]Card, 0, len(ids)*2)
 	for _, id := range ids {
-		c, err := s.Build(id, lang)
+		r, err := s.Get(id, lang)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("getting ref %d: %s", id, err), http.StatusInternalServerError)
+			return
+		}
+
+		c, err := s.Build(r)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("building ref %d: %s", id, err), http.StatusInternalServerError)
 			return
 		}
+
 		cards = append(cards, c...)
 	}
 
@@ -114,50 +106,6 @@ func (s *Service) DisplayEndpoint(w http.ResponseWriter, r *http.Request, p http
 		"slug": func(s string) string {
 			return strings.Replace(s, " ", "-", -1)
 		},
-		// compile takes a strings and returns a HTML version of it
-		// with abilities and advantages tags replaced by their textual
-		// versions.
-		"compile": func(src string) (string, error) {
-			log15.Debug("compiling", "src", src)
-			var abilities []string
-			var err *multierror.Error
-
-			var buf strings.Builder
-			buf.WriteString(reAbility.ReplaceAllStringFunc(src, func(tag string) string {
-				id, e := strconv.Atoi(tag[1:strings.IndexRune(tag, ':')])
-				if e != nil {
-					err = multierror.Append(err, e)
-					log15.Debug("parsing ability tag", "tag", tag, "err", e)
-					return tag
-				}
-
-				ability, e := s.src.Ability.Get(id, lang)
-				if e != nil {
-					err = multierror.Append(err, e)
-					log15.Debug("finding ability", "tag", tag, "err", e)
-					return tag
-				}
-
-				abilities = append(abilities, fmt.Sprintf("%s: %s", ability.Name, ability.Description))
-				return ability.Name
-			}))
-			for _, ability := range abilities {
-				buf.WriteString(fmt.Sprintf(" _(%s)_", ability))
-			}
-
-			res := reAdvantage.ReplaceAllStringFunc(buf.String(), func(tag string) string {
-				advantage, e := s.src.Advantage.Get(tag[1 : len(tag)-1])
-				if e != nil {
-					err = multierror.Append(err, e)
-					log15.Debug("finding advantage", "tag", tag, "err", e)
-					return tag
-				}
-
-				return fmt.Sprintf(`%s ![%s](%s/icons/%s.png)`, advantage.Name, advantage.ID, s.assets, advantage.ID)
-			})
-
-			return markdown.New().RenderToString([]byte(res)), err.ErrorOrNil()
-		},
 	}).ParseFiles(path.Join(s.assets, "templates/cards.html"))
 	if err != nil {
 		log.Error("parsing card template", "err", err)
@@ -171,148 +119,4 @@ func (s *Service) DisplayEndpoint(w http.ResponseWriter, r *http.Request, p http
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
-
-func (s *Service) Build(id int, lang string) (cards []Card, err error) {
-	ref, err := s.src.Ref.Get(id, lang)
-	if err != nil {
-		return nil, fmt.Errorf(`fetching reference: %w`, err)
-	}
-
-	profile := ProfileCard{
-		Ref:           ref,
-		ModelsWeapons: make(map[int][]weapon.Weapon),
-	}
-
-	rules := RulesCard{
-		Ref:              ref,
-		ModelsAbilities:  make(map[int][]ability.Ability),
-		ModelsWeapons:    make(map[int][]weapon.Weapon),
-		WeaponsAbilities: make(map[int][]ability.Ability),
-	}
-
-	rules.RefAbilities, err = s.src.Ability.ListByRef(ref.ID, lang)
-	if err != nil {
-		return nil, fmt.Errorf(`fetching abilities for ref %d: %w`, ref.ID, err)
-	}
-
-	models, err := s.src.Model.List(id, lang)
-	if err != nil {
-		return nil, fmt.Errorf(`fetching models for ref%d: %w`, ref.ID, err)
-	}
-	profile.Models = models
-	rules.Models = models
-
-	for _, model := range models {
-		abilities, err := s.src.Ability.ListByModel(model.ID, lang)
-		if err != nil {
-			return nil, fmt.Errorf(`fetching abilities for model %d: %w`, model.ID, err)
-		}
-		rules.ModelsAbilities[model.ID] = abilities
-
-		weapons, err := s.src.Weapon.List(model.ID, lang)
-		if err != nil {
-			return nil, fmt.Errorf(`fetching weapons for model %d: %w`, model.ID, err)
-		}
-		profile.ModelsWeapons[model.ID] = weapons
-		rules.ModelsWeapons[model.ID] = weapons
-
-		for _, weapon := range weapons {
-			abilities, err := s.src.Ability.ListByWeapon(weapon.ID, lang)
-			if err != nil {
-				return nil, fmt.Errorf(`fetching abilities for weapon %d: %w`, weapon.ID, err)
-			}
-			rules.WeaponsAbilities[weapon.ID] = abilities
-		}
-	}
-
-	cards = append(cards, profile, rules)
-
-	if ref.HasSpells() {
-		spells, err := s.src.Spell.ListByRef(ref.ID, lang)
-		if err != nil {
-			return nil, fmt.Errorf(`fetching spells for ref %d: %w`, ref.ID, err)
-		}
-
-		cards = append(cards, SpellsCard{
-			Ref:    ref,
-			Spells: spells,
-		})
-	}
-
-	if ref.HasFeat() {
-		feat, err := s.src.Feat.Get(ref.ID, lang)
-		if err != nil {
-			return nil, fmt.Errorf(`fetching feat: %w`, err)
-		}
-
-		cards = append(cards, FeatCard{
-			Ref:  ref,
-			Feat: feat,
-		})
-	}
-
-	return cards, nil
-}
-
-type Card interface {
-	Type() string
-	Faction() reference.Faction
-}
-
-type ProfileCard struct {
-	Ref           *reference.Reference
-	Models        []model.Model
-	ModelsWeapons map[int][]weapon.Weapon
-}
-
-func (c ProfileCard) Faction() reference.Faction {
-	return c.Ref.FactionID
-}
-
-func (ProfileCard) Type() string {
-	return "profile"
-}
-
-type RulesCard struct {
-	Ref              *reference.Reference
-	RefAbilities     []ability.Ability
-	Models           []model.Model
-	ModelsAbilities  map[int][]ability.Ability
-	ModelsWeapons    map[int][]weapon.Weapon
-	WeaponsAbilities map[int][]ability.Ability
-}
-
-func (RulesCard) Type() string {
-	return "rules"
-}
-
-func (c RulesCard) Faction() reference.Faction {
-	return c.Ref.FactionID
-}
-
-type SpellsCard struct {
-	Ref    *reference.Reference
-	Spells []spell.Spell
-}
-
-func (c SpellsCard) Faction() reference.Faction {
-	return c.Ref.FactionID
-}
-
-func (SpellsCard) Type() string {
-	return "spells"
-}
-
-type FeatCard struct {
-	Ref  *reference.Reference
-	Feat *feat.Feat
-}
-
-func (c FeatCard) Faction() reference.Faction {
-	return c.Ref.FactionID
-}
-
-func (FeatCard) Type() string {
-	return "feat"
 }
