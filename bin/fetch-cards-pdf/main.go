c7a697fc0b9e8c7f34046e24517ff959943625b7
Emilie Sicilia <emilie.sicilia@qonto.eu>
2020-07-01 13:37:35 +0200

    fix error handling (i hope so)

--- bin/fetch-cards-pdf/main.go
+++ bin/fetch-cards-pdf/main.go
@@ -35,7 +35,7 @@ func main() {
 	cfg := config{}
 
 	flag.StringVar(&cfg.dsn, "dsn", "cards_api:cards_api@tcp(localhost:3306)/cards_db", "DSN of the Jackmarshall Database")
-	flag.StringVar(&cfg.ppURL, "base-url", "http://cards.privateerpress.com", "URL of the Privateer Press Cards Database")
+	flag.StringVar(&cfg.ppURL, "pp-url", "http://cards.privateerpress.com", "URL of the Privateer Press Cards Database")
 	flag.StringVar(&cfg.destDir, "dest-dir", ".", "directory to output the images into")
 	flag.IntVar(&cfg.workers, "workers", 10, "number of cards to process in parallel")
 	flag.BoolVar(&cfg.secondaryOnly, "secondary-only", false, "disable download of the first cards")
@@ -54,18 +54,21 @@ func main() {
 
 	// create job queue
 	jobQueue := make(chan DownloadJob)
+	errQueue := make(chan error)
 	go func() {
 		// queue front cards for all refs
 		if !cfg.secondaryOnly {
-			queueFirstCards(cfg.ppURL, jobQueue)
+			queueFirstCards(cfg.ppURL, jobQueue, errQueue)
 		}
 
 		// queue front cards for special cases (colossal, character unit, dragoon)
-		queueSpecialCaseCards(db, jobQueue)
+		queueSpecialCaseCards(db, jobQueue, errQueue)
 
 		// queue front cards for attachments (makeda & exalted court)
-		queueAttachmentCards(db, jobQueue)
+		queueAttachmentCards(db, jobQueue, errQueue)
+
 		close(jobQueue)
+		close(errQueue)
 	}()
 
 	// process jobQueue
@@ -77,31 +80,41 @@ func main() {
 			log.Debug("starting worker", "w", w)
 
 			for job := range jobQueue {
-				if err := downloadCard(cfg.ppURL, cfg.destDir, job.RefID, job.Index); err != nil {
-					log.Error("extract card", "err", err)
+				err := downloadCard(cfg.ppURL, cfg.destDir, job)
+				if err != nil {
+					log.Error("downloading card", "err", err, "refId", job.RefID, "index", job.Index)
 					continue
 				}
 			}
 		}(w)
 	}
-	wg.Wait()
 
-	log.Debug("done")
+	// process errorQueue
+	wg.Add(1)
+	go func() {
+		for err := range errQueue {
+			log.Error("error while queuing refs", "err", err)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	log.Info("job done")
 }
 
-func queueFirstCards(ppURL string, jobQueue chan DownloadJob) {
+func queueFirstCards(ppURL string, jobQueue chan DownloadJob, errQueue chan error) {
 	log.Debug("retrieving PP cards database")
 	res, err := http.Get(ppURL)
 	if err != nil {
-		log.Error("retrieving pp cards database", "err", err)
-		os.Exit(1)
+		errQueue <- errors.Wrap(err, "retrieving pp cards database")
+		return
 	}
 
 	log.Debug("parsing PP cards database")
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Error("parsing pp cards database", "err", err)
-		os.Exit(1)
+		errQueue <- errors.Wrap(err, "parsing pp cards database")
+		return
 	}
 
 	log.Debug("looking for refs")
@@ -114,15 +127,14 @@ func queueFirstCards(ppURL string, jobQueue chan DownloadJob) {
 	})
 }
 
-func queueSpecialCaseCards(db *sqlx.DB, jobQueue chan DownloadJob) {
+func queueSpecialCaseCards(db *sqlx.DB, jobQueue chan DownloadJob, errQueue chan error) {
 	log.Debug("looking for special cases")
-
 	stmt := `SELECT id, ppid, special FROM refs WHERE special != ""`
 	refs := []reference.Reference{}
 	err := db.Select(&refs, stmt)
 	if err != nil {
-		log.Error("fetch special cases", "err", err)
-		os.Exit(1)
+		errQueue <- errors.Wrap(err, "fetching special cases")
+		return
 	}
 
 	for _, ref := range refs {
@@ -131,12 +143,11 @@ func queueSpecialCaseCards(db *sqlx.DB, jobQueue chan DownloadJob) {
 			Index: 1,
 		}
 	}
-	log.Debug("done queuing special cases", "count", len(refs))
+	log.Info("done queuing special cases", "count", len(refs))
 }
 
-func queueAttachmentCards(db *sqlx.DB, jobQueue chan DownloadJob) {
+func queueAttachmentCards(db *sqlx.DB, jobQueue chan DownloadJob, errQueue chan error) {
 	log.Debug("looking for attachements ")
-
 	stmt := `
 		SELECT parent.ppid, parent.category_id, parent.special
 		FROM (select linked_to FROM refs WHERE linked_to is not null AND linked_to != 0) as ref
@@ -145,8 +156,8 @@ func queueAttachmentCards(db *sqlx.DB, jobQueue chan DownloadJob) {
 	refs := []reference.Reference{}
 	err := db.Select(&refs, stmt)
 	if err != nil {
-		log.Error("fetch attachementss", "err", err)
-		os.Exit(1)
+		errQueue <- errors.Wrap(err, "fetching attachements")
+		return
 	}
 
 	for _, ref := range refs {
@@ -166,18 +177,18 @@ func queueAttachmentCards(db *sqlx.DB, jobQueue chan DownloadJob) {
 			}
 		}
 	}
-	log.Debug("done queuing attachements", "count", len(refs))
-
+	log.Info("done queuing attachements", "count", len(refs))
 }
 
-func downloadCard(ppURL, destDir, id string, index int) error {
+func downloadCard(ppURL, destDir string, job DownloadJob) error {
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 
-	log := log.New("ref", id)
+	log := log.New("ref", job.RefID)
 	log.Debug("retrieving card")
 
-	res, err := http.Get(fmt.Sprintf("%s/?card_items_to_pdf=$%s,1", ppURL, id))
+	url := fmt.Sprintf("%s/?card_items_to_pdf=$%s,1", ppURL, job.RefID)
+	res, err := http.Get(url)
 	if err != nil {
 		return errors.Wrap(err, "retrieving card")
 	}
@@ -214,16 +225,16 @@ func downloadCard(ppURL, destDir, id string, index int) error {
 	card_h := 1050
 	card_w := 748
 	space := 7
-	err = mw.CropImage(uint(card_w), uint(card_h), 140+(card_w+space)*index, 227)
+	err = mw.CropImage(uint(card_w), uint(card_h), 140+(card_w+space)*job.Index, 227)
 	if err != nil {
 		return errors.Wrap(err, "extracting card")
 	}
 
 	var path string
-	if index == 0 {
-		path = fmt.Sprintf("%s/%s.png", destDir, id)
+	if job.Index == 0 {
+		path = fmt.Sprintf("%s/%s.png", destDir, job.RefID)
 	} else {
-		path = fmt.Sprintf("%s/%s_%d.png", destDir, id, index)
+		path = fmt.Sprintf("%s/%s_%d.png", destDir, job.RefID, job.Index)
 	}
 	err = mw.WriteImage(path)
 	if err != nil {
